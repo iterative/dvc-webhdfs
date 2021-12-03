ac22a0d9ea6c97fd95effcb63aa1c8a7f7b9c14e
Ruslan Kuprieiev <kupruser@gmail.com>
2021-12-03 09:45:39 +0200

    tests: don't forget docker_compose_file

--- dvc_webhdfs/tests/conftest.py
+++ dvc_webhdfs/tests/conftest.py
@@ -1,5 +1,6 @@
 from dvc.testing.fixtures import *  # noqa, pylint: disable=wildcard-import,unused-import
 from dvc_hdfs.tests.fixtures import (  # noqa, pylint: disable=unused-import
+    docker_compose_file,
     hadoop,
     hdfs_server,
 )
